0f8e76368f5d3c885d767185ae8028efce083795
jpowell11 <deyoda365@gmail.com>
2024-05-10 06:56:18 -0500

    Secrets management

--- webapp/main.py
+++ webapp/main.py
@@ -1,10 +1,21 @@
 from typing import Any
 import uuid
-from fastapi.responses import HTMLResponse
 import uvicorn
 import urllib.parse
+import os
+from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from sqlalchemy import Column, Connection, Engine, Integer, MetaData, Row, Sequence, Table, Text, Tuple, create_engine, insert, select, text
+
+load_dotenv(find_dotenv(filename='.env', raise_error_if_not_found=True, usecwd=False))
+POSTGRES_DB_NAME=os.environ.get("POSTGRES_DB_NAME")
+POSTGRES_DB_USER=os.environ.get("POSTGRES_DB_USER")
+POSTGRES_DB_PASS=os.environ.get("POSTGRES_DB_PASS")
+POSTGRES_DB_HOST=os.environ.get("POSTGRES_DB_HOST")
+POSTGRES_DB_PORT=os.environ.get("POSTGRES_DB_PORT")
+APP_HOST=os.environ.get("APP_HOST")
+APP_PORT=os.environ.get("APP_PORT")
 
 app = FastAPI()
 metadata: MetaData = MetaData()
@@ -24,7 +35,7 @@ async def shortenURL(longurl: str):
 
     longurl = urllib.parse.unquote(longurl) #maybe unneeded
 
-    engine: Engine = create_engine("postgresql+psycopg2://postgres:postgres@db:5432/pynyurl")
+    engine: Engine = create_engine("postgresql+psycopg2://%s:%s@%s:%s/%s" % (POSTGRES_DB_USER, POSTGRES_DB_PASS, POSTGRES_DB_HOST, POSTGRES_DB_PORT, POSTGRES_DB_NAME))
     conn: Connection = engine.connect()
     shorturl: str = ''
     res: int = -1
@@ -71,5 +82,5 @@ async def redirect_user(shorturl: str):
     """ % res[0]
 
 if __name__ == "__main__":
-   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+   uvicorn.run("main:app", host=APP_HOST, port=APP_PORT, reload=True)
 
