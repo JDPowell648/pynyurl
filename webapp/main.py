2cfe73ff2b64b7c5734d889c18246173da040b72
jpowell11 <deyoda365@gmail.com>
2024-05-10 16:53:07 -0500

    Files formatted

--- webapp/main.py
+++ webapp/main.py
@@ -6,45 +6,76 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from sqlalchemy import Column, Connection, Engine, Integer, MetaData, Row, Sequence, Table, Text, Tuple, create_engine, insert, select, text
+from sqlalchemy import (
+    Column,
+    Connection,
+    Engine,
+    Integer,
+    MetaData,
+    Row,
+    Sequence,
+    Table,
+    Text,
+    Tuple,
+    create_engine,
+    insert,
+    select,
+    text,
+)
 
-POSTGRES_DB_NAME=os.environ['POSTGRES_DB_NAME']
-POSTGRES_DB_USER=os.environ['POSTGRES_DB_USER']
-POSTGRES_DB_PASS=os.environ['POSTGRES_DB_PASS']
-POSTGRES_DB_HOST=os.environ['POSTGRES_DB_HOST']
-POSTGRES_DB_PORT=os.environ['POSTGRES_DB_PORT']
-APP_HOST=os.environ['APP_HOST']
-APP_PORT=os.environ['APP_PORT']
+POSTGRES_DB_NAME = os.environ["POSTGRES_DB_NAME"]
+POSTGRES_DB_USER = os.environ["POSTGRES_DB_USER"]
+POSTGRES_DB_PASS = os.environ["POSTGRES_DB_PASS"]
+POSTGRES_DB_HOST = os.environ["POSTGRES_DB_HOST"]
+POSTGRES_DB_PORT = os.environ["POSTGRES_DB_PORT"]
+APP_HOST = os.environ["APP_HOST"]
+APP_PORT = os.environ["APP_PORT"]
 
 app = FastAPI()
 metadata: MetaData = MetaData()
 urls = Table(
-        'urls',
-        metadata,
-        Column("shorturl", Text, nullable=False, primary_key=True),
-        Column("longurl", Text, nullable=False),
-        Column("interactions", Integer),
-    )
+    "urls",
+    metadata,
+    Column("shorturl", Text, nullable=False, primary_key=True),
+    Column("longurl", Text, nullable=False),
+    Column("interactions", Integer),
+)
 
-@app.get("/new/") 
+
+@app.get("/new/")
 async def shortenURL(longurl: str):
-    #This will generate a shortened URL from a URL that a user delivers to it
-    #Pretty sure this should be a PUT or POST
-    #Add success/fails
+    # This will generate a shortened URL from a URL that a user delivers to it
+    # Pretty sure this should be a PUT or POST
+    # Add success/fails
 
-    longurl = urllib.parse.unquote(longurl) #maybe unneeded
+    longurl = urllib.parse.unquote(longurl)  # maybe unneeded
 
-    engine: Engine = create_engine("postgresql+psycopg2://%s:%s@%s:%s/%s" % (POSTGRES_DB_USER, POSTGRES_DB_PASS, POSTGRES_DB_HOST, POSTGRES_DB_PORT, POSTGRES_DB_NAME))
+    engine: Engine = create_engine(
+        "postgresql+psycopg2://%s:%s@%s:%s/%s"
+        % (
+            POSTGRES_DB_USER,
+            POSTGRES_DB_PASS,
+            POSTGRES_DB_HOST,
+            POSTGRES_DB_PORT,
+            POSTGRES_DB_NAME,
+        )
+    )
     conn: Connection = engine.connect()
-    shorturl: str = ''
+    shorturl: str = ""
     res: int = -1
-    while(res != 0):
-        shorturl = uuid.uuid4().hex[0:7] #probably should do it better than this! placeholder
-        query: Text = text("SELECT COUNT(longurl) FROM urls WHERE shorturl = '%s'" % (shorturl))
+    while res != 0:
+        shorturl = uuid.uuid4().hex[
+            0:7
+        ]  # probably should do it better than this! placeholder
+        query: Text = text(
+            "SELECT COUNT(longurl) FROM urls WHERE shorturl = '%s'" % (shorturl)
+        )
         res = conn.execute(query).fetchone()[0]
         conn.commit()
 
-    query: Text = text("INSERT INTO urls (longurl, shorturl) VALUES ('%s','%s');" % (longurl, shorturl))
+    query: Text = text(
+        "INSERT INTO urls (longurl, shorturl) VALUES ('%s','%s');" % (longurl, shorturl)
+    )
 
     conn.execute(query)
     conn.commit()
@@ -52,23 +83,30 @@ async def shortenURL(longurl: str):
 
     return {"shorturl": shorturl, "longurl": longurl}
 
+
 @app.get("/use/{shorturl}", response_class=HTMLResponse)
 async def redirect_user(shorturl: str):
-    #Redirect the user to the long URL from the short URL
-    #Add success/fails
-    engine: Engine = create_engine("postgresql+psycopg2://%s:%s@%s:%s/%s" % (POSTGRES_DB_USER, POSTGRES_DB_PASS, POSTGRES_DB_HOST, POSTGRES_DB_PORT, POSTGRES_DB_NAME))
+    # Redirect the user to the long URL from the short URL
+    # Add success/fails
+    engine: Engine = create_engine(
+        "postgresql+psycopg2://%s:%s@%s:%s/%s"
+        % (
+            POSTGRES_DB_USER,
+            POSTGRES_DB_PASS,
+            POSTGRES_DB_HOST,
+            POSTGRES_DB_PORT,
+            POSTGRES_DB_NAME,
+        )
+    )
     conn: Connection = engine.connect()
 
-    query = select(
-        urls.columns.longurl
-    ).where(
-        urls.columns.shorturl == shorturl
-    )
+    query = select(urls.columns.longurl).where(urls.columns.shorturl == shorturl)
 
     res: Row[Tuple[Any]] = conn.execute(query).fetchone()
     conn.close()
 
-    return """
+    return (
+        """
     <html>
         <body>
             <script>
@@ -78,8 +116,10 @@ async def redirect_user(shorturl: str):
             </script>
         </body>
     </html>
-    """ % res[0]
+    """
+        % res[0]
+    )
+
 
 if __name__ == "__main__":
-   uvicorn.run("main:app", host=APP_HOST, port=APP_PORT, reload=True)
-
+    uvicorn.run("main:app", host=APP_HOST, port=APP_PORT, reload=True)
